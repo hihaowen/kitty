ec4d92f03148fcf434990e4a9c8113768c53c34c
haowenzhi <haowenzhi@dfs800.com>
2022-06-02 20:49:34 +0800

    Add GET实现

--- main.go
+++ main.go
@@ -6,7 +6,19 @@ import (
 	"log"
 	"net"
 	"os"
+	"strings"
 )
+
+type getMap struct {
+	val map[string]string
+}
+
+var getMaps getMap
+
+func init() {
+	getMaps = *new(getMap)
+	getMaps.val = map[string]string{}
+}
 
 func main() {
 
@@ -30,26 +42,62 @@ func main() {
 }
 
 func handleClient(conn net.Conn) {
-	var buf [1]byte
 	for {
+		var buf [512]byte
 		n, err := conn.Read(buf[:])
 		if err != nil {
 			return
 		}
 
 		reqBytes := buf[:]
+		reqBytes = bytes.TrimRight(reqBytes, "\x00\x0a\x0d")
 		reqString := string(reqBytes)
 
-		log.Println(n, buf[:], reqString)
+		log.Printf("用户输入 %d 个字节: %s", n, reqString)
+		log.Println(reqBytes)
 
-		if bytes.Compare(buf[:], []byte("quit")) == 0 {
-			log.Println(buf)
+		if reqString == "quit" {
+			log.Println("退出咯...")
 			return
 		}
 
-		_, err2 := conn.Write(reqBytes)
-		if err2 != nil {
-			return
+		args := strings.Split(reqString, " ")
+		if len(args) < 2 {
+			conn.Write([]byte("\n"))
+			continue
+		}
+
+		cmd := args[0]
+		key := args[1]
+
+		log.Println(args, len(args))
+
+		switch {
+		case strings.ToUpper(cmd) == "GET":
+			val, isSet := getMaps.val[key]
+			if ! isSet {
+				conn.Write([]byte("key is not set: " + key + "\n"))
+				break
+			}
+			conn.Write([]byte(val + "\n"))
+			break
+		case strings.ToUpper(cmd) == "SET":
+			if len(args) < 3 {
+				conn.Write([]byte("value is not set: " + key + "\n"))
+				break
+			}
+			getMaps.val[key] = args[2]
+			conn.Write([]byte("ok\n"))
+			break
+		case strings.ToUpper(cmd) == "DEL":
+			_, isSet := getMaps.val[key]
+			if ! isSet {
+				conn.Write([]byte("key is not set: " + key + "\n"))
+				break
+			}
+			delete(getMaps.val, key)
+			conn.Write([]byte("ok\n"))
+			break
 		}
 	}
 }
